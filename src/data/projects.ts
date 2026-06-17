e1c06ff0fe9fca4aacd6dd8c4bf50d79b8bf264c
Rohith Ram <rohithram5467@gmail.com>
2026-06-17 17:01:56 +0530

    updated my portfolio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,7 @@
 import type { Project } from '@/types';
 import pathalyticsCover from '@/assets/projects/pathalytics-cover.png';
 import mextCover from '@/assets/projects/mext-tracker-cover.png';
-import PDFRagBot from '@/assets/projects/pdf-rag-bot.png';
+import PDFRagBot from '@/assets/projects/pdf-rag-chatbot-image.png';
 import InvisibilityCloak from '@/assets/projects/invisibility-cloak.png';
 import CareerRoadmap from '@/assets/projects/career-roadmap.png';
 import NihongoPlatform from '@/assets/projects/nihongo-platform.png';
